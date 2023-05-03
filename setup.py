620632e43d2f5ec6262364c6ca5e2dfdc19bf907
AndrewM <105721308+AndrewMaraiev@users.noreply.github.com>
2023-05-03 15:41:05 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+# get version from __version__ variable in erpnext_shipping/__init__.py
+from erpnext_shipping import __version__ as version
+
+setup(
+	name='erpnext_novaposhta',
+	version=version,
+	description='A NP Shipping Integration for ERPNext',
+	author='ikrok',
+	author_email='maraiev.a@ikrok.net',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=''
+)
